dfd98e23c5cb7fa8a17f21a69f6ecda3f2d55f1e
bilelhn <hannafi.bilel@gmail.com>
2024-03-31 00:27:37 +0000

    factorial function

--- script.js
+++ script.js
@@ -92,3 +92,14 @@
 
 
 
+
+//     Mathematical Functions ----------
+
+// 1 - Factorial -------------
+        // function fact(number){
+        
+        //         if (number === 0){
+        //          return 1;
+        //         }  return number * fact(number-1);
+        // }
+        // console.log(fact(3));
